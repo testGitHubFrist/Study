38b05ffa4ce32b051285568e45dbc2faf5f5b32c
shanchuang.zhang@ly.com <shanchuang.zhang@ly.com>
2019-02-27 16:07:08 +0800

    新增ServletContextListener

---
+++ src/main/java/com/zsc/study/listener/HostRegisterListener.java
@@ -0,0 +1,22 @@
+package com.zsc.study.listener;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+/**
+ * @Auther: zhangshanchuang
+ * @Date: 19/2/27 16:04
+ * @Description: 向zk注册服务
+ */
+public class HostRegisterListener implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+
+    }
+}
